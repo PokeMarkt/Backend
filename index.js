e8f26dba4f544cad19ad3c39e1bc8f58af8f7b35
joseffffff <joseffff@protonmail.com>
2018-07-15 23:44:06 +0200

    Project structure

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 'use strict'
 
-console.log('Hey')
+const morgan = require('morgan')
+const express = require('express')
+
+const app = express();
+const router = express.Router();
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use('/', router);
+
+require('./routes/pokemons')(router)
+
+// Starting the server
+app.listen(80, function() {
+    console.log('Server running on port 80')
+})
